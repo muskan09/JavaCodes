4ca7c2c13973e747e9b1a9e7e0cbc40629c3bac2
muskan09 <muskankalsi9@gmail.com>
2018-11-15 23:06:12 +0530

    Java Codes

---
+++ sample.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class First
+{
+    {
+       System.out.println(10);
+     }
+}
+class Second extends First
+{
+    {
+       System.out.println(20);
+     }
+}
+class Third extends Second
+{
+    {
+       System.out.println(30);
+     }
+}
+class sample{
+public static void main(String args[])
+{
+Third t = news Third();
+}
+}
