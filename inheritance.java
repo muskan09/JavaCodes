06281d7d438b53aadf36bbde3c7441693dd09719
muskan09 <muskankalsi9@gmail.com>
2018-11-15 23:04:03 +0530

    Java Codes

---
+++ inheritance.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+class CreditCard{
+Private:
+  String number;
+  String holdername;
+  double amount;
+  Public:
+
+}
+class TravelCreditCard extends CreditCard{
+Private:
+double  exchangePercentage;
+Public:
+}
+class RewardsCreditCard extends CreditCard{
+Private:
+double creditPoints;
+Public:
+
+}
+class InternationalCard extends TravelCreditCard{
+
+}
+class CountryCard extends TravelCreditCard{
+
+}
+
+class inheritance{
+  public static void main(String args[]){
+
+  }
+
+}
