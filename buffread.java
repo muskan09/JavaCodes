bc855d2b2311833a094646126663b4cf619d3aa4
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:57:15 +0530

    Java Codes

---
+++ buffread.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+class buffread{
+  public static void main(String args[])throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter char string int");
+
+    char ch=(char)br.read();
+    br.readLine();
+    String s=br.readLine();
+
+    int i=Integer.parseInt(br.readLine());
+    System.out.println("String:"+s);
+    System.out.println("Char:"+ch);
+    System.out.println("Int:"+i);
+  }
+}
