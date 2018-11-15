9d3b2838786d95b4a520b4872efee28e73ff22c4
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:56:29 +0530

    Java Codes

---
+++ anagram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io;
+class anagram{
+  public static void main(String args[]){
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter string 1");
+    String s1= br.readLine();
+    System.out.println("Enter string 2");
+    String s2=br.readLine();
+    int x=s1.length();
+    int y=s2.length();
+    if(x==y){
+      for(int i=0;i<x;i++){
+        for(int j=0;j<y;j++){
+          if(s1.charAt()
+        }
+      }
+    }
+      else{
+        System.out.println("It's not an anagram");
+      }
+  }
+}
