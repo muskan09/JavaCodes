7c55a83de99a9db723b82f825a2fd59fdda6bbd0
muskan09 <muskankalsi9@gmail.com>
2018-11-15 23:05:05 +0530

    Java Codes

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+/*class o{
+int x=2121;
+}
+class Two{
+int x=1212;
+{
+System.out.println(x);
+}
+}*/
+public class Main{
+public static void main(String args[]){
+//Two two=new Two();
+int arr[]={10,20,30,40,50};
+for(int i=0;i<arr.length;i++){
+System.out.println(""+arr[i]);
+}
+}
+}
