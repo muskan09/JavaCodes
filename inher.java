76b4d2faf8b4864851d9f98e6f18f951108de1b6
muskan09 <muskankalsi9@gmail.com>
2018-11-15 23:04:08 +0530

    Java Codes

---
+++ inher.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+class CDimension{
+  int length;
+  int width;
+  CDimension(int length,int width){
+    this.length=l;
+    this.width=w;
+  }
+}
+class CArea{
+  int ar;
+  CArea(int length,int breadth){
+    super(length,breadth);
+  }
+  void Area{
+    ar=length*width;
+  }
+}
+class CVolume{
+  int height,volume;
+  
+}
